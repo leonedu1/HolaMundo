1036998d82c93c9aa845147268c7790f04f213d5
DarioE <dario.leon.m@uni.pe>
2024-11-11 20:19:34 -0500

    Fibonacci

---
+++ fibo.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+def main():
+    n = int(input("Introduce la cantidad de términos de la secuencia de Fibonacci que deseas ver: "))
+    print("Secuencia de Fibonacci:")
+    for val in fibonacci(n):
+        print(val)
+
+if __name__ == "__main__":
+    main()
